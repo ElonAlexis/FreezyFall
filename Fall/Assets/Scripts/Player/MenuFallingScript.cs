fc77639b564035a6a10fa949c971086998266df0
KingZoldack <jeanpaulismael@gmail.com>
2023-08-04 01:13:20 +0100

    Main Menu - Player
    Add random start to player

--- Fall/Assets/Scripts/Player/MenuFallingScript.cs
+++ Fall/Assets/Scripts/Player/MenuFallingScript.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class MenuFallingScript : MonoBehaviour
 {
-    float minY = -0.541f, maxY = 3f;
-    float movesSpeed =0.5f;
-    float minX = -0.241f, maxX = 0.247f;
+    [SerializeField] float minY = -0.541f, maxY = 3f;
+    [SerializeField] float movesSpeed =0.5f;
+    [SerializeField] float minX = -0.241f, maxX = 0.247f;
+
+    Vector2 position;
+    
+    private void Start()
+    {
+        position = transform.position;
+        position.x = Random.Range(minX, maxX);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
         position.y -= movesSpeed * Time.deltaTime;
         
         if (position.y < minY)
